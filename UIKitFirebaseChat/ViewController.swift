e37672333235ecf3e26a778778fdfba20fc8852a
Ray Yu <s1174159@ouhk.edu.hk>
2023-02-09 19:29:39 -0800

    Initial Commit

---
+++ UIKitFirebaseChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitFirebaseChat
+//
+//  Created by yu fai on 9/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
